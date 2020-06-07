067b22afecc524fdf9d2896c384865e7e6af2241
holzmaster <holzmaster@outlook.com>
2020-06-07 19:44:43 +0200

    Remove list styling in stats

--- augenzuckerl.user.js
+++ augenzuckerl.user.js
@@ -86,46 +86,4 @@ input[type=radio].box-from-label + label::before {
 .subtitle {
 	color: #888; /* the original uses the primary color everywhere, which is pretty heavy */
 }
-
-.list-comments .thumb,
-.list-items .thumb {
-	width: 128px;
-	text-align: center;
-}
-
-.list-comments > div > .votes,
-.list-items > div > .votes {
-	font-size: 18px !important; /* site uses style attributes here */
-}
-.list-comments > div > .thumb img,
-.list-items > div > .thumb img {
-	width: 48px;
-}
-
-.list-comments > div:first-child > .votes,
-.list-items > div:first-child > .votes {
-	font-size: 40px !important; /* site uses style attributes here */
-}
-.list-comments > div:first-child > .thumb img,
-.list-items > div:first-child > .thumb img {
-	width: 128px;
-}
-
-.list-comments > div:nth-child(2) > .votes,
-.list-items > div:nth-child(2) > .votes {
-	font-size: 30px !important; /* site uses style attributes here */
-}
-.list-comments > div:nth-child(2) > .thumb img,
-.list-items > div:nth-child(2) > .thumb img {
-	width: 92px;
-}
-
-.list-comments > div:nth-child(3) > .votes,
-.list-items > div:nth-child(3) > .votes {
-	font-size: 24px !important; /* site uses style attributes here */
-}
-.list-comments > div:nth-child(3) > .thumb img,
-.list-items > div:nth-child(3) > .thumb img {
-	width: 64px;
-}
 `);
