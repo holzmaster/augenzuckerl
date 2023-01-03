4f55ce4d3a9bcc03aef39d096408c74144745ab2
holzmaster <15527285+holzmaster@users.noreply.github.com>
2023-01-03 02:02:02 +0100

    Bump version

--- augenzuckerl.user.js
+++ augenzuckerl.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name        Augenzuckerl
-// @version     1.9.0
+// @version     2.0.0
 // @author      holzmaster
 // @namespace   holzmaster
 // @include     https://pr0gramm.com*
